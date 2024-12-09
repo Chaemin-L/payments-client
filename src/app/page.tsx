d6b92e6875bdc485b7c0ed78b7b994829f2db639
Chaemin-L <chaem12@kookmin.ac.kr>
2024-12-09 20:14:42 +0900

    feat: token 전송 로직 임시 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import PaymentsInfo from "./payments-info";
+import fetchClient from "@/lib/fetchClient";
+import PaymentsInfo from "@/app/payments-info";
 
-export default function Home() {
+interface Props {
+  searchParams: Promise<{ payments: string }>;
+}
+
+export default async function Home({ searchParams }: Props) {
+  const { payments: token } = await searchParams;
+  const data = fetchClient(`/${token}`);
+  console.log(data);
+
   const savedPoint = 110;
 
   return (
--- src/lib/fetchClient.ts
+++ src/lib/fetchClient.ts
@@ -15,7 +15,11 @@ const fetchClient = async (url: string, options: RequestInit = {}) => {
     },
   };
 
-  const response = await fetch(url, mergedOptions);
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const response = await fetch(
+    `${BASE_URL}/v1/pay/payments${url}`,
+    mergedOptions
+  );
   // 통신 에러
   if (!response.ok) {
     throw new Error(`${response.status} ${response.statusText}`);
